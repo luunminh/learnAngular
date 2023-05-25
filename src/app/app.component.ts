8267b8aa74dfa5fc1f3f01fc9ab46af1ec998dab
minhluu.24hdev <minhluu.24hdev@gmail.com>
2023-05-25 22:05:45 +0700

    gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { Logger } from './logger.service';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'My very first Project';
-  message = 'Hello Angular';
+    title = 'My very first Project';
+    message = 'Hello Angular';
 
-  constructor(private logger: Logger) {}
+    constructor(private logger: Logger) {}
 
-  onAlertMsg() {
-    this.logger.showMsg(this.message);
-  }
+    onAlertMsg() {
+        this.logger.showMsg(this.message);
+    }
 }
