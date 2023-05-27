17fad16afee1cce685c474d9ad1cd6707c6ed478
luunminh <lnm16092002@gmail.com>
2023-05-27 17:34:14 +0700

    Done header navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { Logger } from './logger.service';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'My very first Project';
-  message = 'Hello Angular';
+    feature: string;
 
-  constructor(private logger: Logger) {}
+    constructor() {
+        this.feature = 'recipe';
+    }
 
-  onAlertMsg() {
-    this.logger.showMsg(this.message);
-  }
+    onNavigate(featureName: string) {
+        this.feature = featureName;
+    }
 }
