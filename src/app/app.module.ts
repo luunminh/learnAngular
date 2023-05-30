559427ebf415ba23945d26c8255f96b6000ea752
luunminh <lnm16092002@gmail.com>
2023-05-30 16:55:16 +0700

    structural directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BasicHighLightDirective } from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
-
+import { UnlessDirective } from './unless.directive';
 @NgModule({
     declarations: [
         AppComponent,
         BasicHighLightDirective,
         BetterHighlightDirective,
+        UnlessDirective,
     ],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
