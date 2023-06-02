dc2107d0f32f2208c48c6209afce99f8800e685d
luunminh <lnm16092002@gmail.com>
2023-06-02 15:08:08 +0700

    handling add ingredients into shopping-list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,3 +1,5 @@
+import { ShoppingListService } from './../../shopping-list/shopping-list.service';
+import { Ingredient } from './../../shared/ingredient.model';
 import { RecipeService } from './../recipe.service';
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
@@ -7,7 +9,13 @@ import { Recipe } from '../recipe.model';
     styleUrls: ['./recipe-detail.component.scss'],
 })
 export class RecipeDetailComponent {
-    @Input('selectedRecipe') recipe: Recipe = new Recipe('', '', '');
+    @Input('selectedRecipe') recipe: Recipe | undefined;
 
-    // constructor(private recipeService : RecipeService) {}
+    constructor(private splService: ShoppingListService) {}
+
+    addRecipeToCart(ingredients: Ingredient[]) {
+        ingredients.forEach((ingredient: Ingredient) => {
+            this.splService.addIngredient(ingredient);
+        });
+    }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,24 +1,26 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 @Injectable({ providedIn: 'root' })
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>()
+    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe(
             'A test recipe',
             'This is a sample test',
-            'https://daylambanh.edu.vn/wp-content/uploads/2020/10/cong-thuc-lam-hamburger.jpg'
+            'https://daylambanh.edu.vn/wp-content/uploads/2020/10/cong-thuc-lam-hamburger.jpg',
+            [new Ingredient('test', 1), new Ingredient('bread', 2)]
         ),
         new Recipe(
             'A Beef Hamburger',
             'This is a new Hamburger',
-            'https://daylambanh.edu.vn/wp-content/uploads/2020/10/cong-thuc-lam-hamburger.jpg'
+            'https://daylambanh.edu.vn/wp-content/uploads/2020/10/cong-thuc-lam-hamburger.jpg',
+            [new Ingredient('meat', 1), new Ingredient('bread', 2)]
         ),
     ];
 
-
     getRecipes() {
-        return this.recipes.slice()
+        return this.recipes.slice();
     }
 }
