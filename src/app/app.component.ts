e7918aa2313200b17c79270987e7e219c10449cf
luunminh <lnm16092002@gmail.com>
2023-06-14 23:49:46 +0700

    Done section 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+    AbstractControl,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    ValidatorFn,
+    Validators,
+} from '@angular/forms';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    status = ['stable', 'critical', 'finished'];
+    projectForm: FormGroup;
+    forbiddenNames = ['minh', 'kiet', 'phong', 'test'];
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.projectForm = new FormGroup({
+            name: new FormControl(null, [
+                Validators.required,
+                forbiddenNameValidator(this.forbiddenNames),
+            ]),
+            email: new FormControl(null, [
+                Validators.email,
+                Validators.required,
+            ]),
+            status: new FormControl(null, [Validators.required]),
+        });
+    }
+
+    onSubmit() {
+        if (this.projectForm.valid) {
+            console.log(this.projectForm.value);
+        } else {
+            alert('Please type valid inputs');
+        }
+    }
+}
+
+function forbiddenNameValidator(name: string[]): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+        const forbidden = name.includes(control.value);
+        return forbidden ? { forbiddenName: { value: control.value } } : null;
+    };
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        FormsModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
