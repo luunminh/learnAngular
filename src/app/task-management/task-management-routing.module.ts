0d909240bbeb750d96221f40934b335100b4138e
luunminh <lnm16092002@gmail.com>
2023-06-17 10:16:46 +0700

    add prettierrc

--- src/app/task-management/task-management-routing.module.ts
+++ src/app/task-management/task-management-routing.module.ts
@@ -4,18 +4,18 @@ import { TaskManagementComponent } from './task-management.component';
 import { CalendarViewComponent } from './components/calendar-view/calendar-view.component';
 import { TaskAddComponent } from './components/tasks/task-add/task-add.component';
 const taskManagementRoutes: Routes = [
-    {
-        path: '',
-        component: TaskManagementComponent,
-        children: [
-            { path: 'calendar-view', component: CalendarViewComponent },
-            { path: 'add', component: TaskAddComponent },
-        ],
-    },
+  {
+    path: '',
+    component: TaskManagementComponent,
+    children: [
+      { path: 'calendar-view', component: CalendarViewComponent },
+      { path: 'add', component: TaskAddComponent },
+    ],
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(taskManagementRoutes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forChild(taskManagementRoutes)],
+  exports: [RouterModule],
 })
 export class TaskManagementRoutingModule {}
